7aebf9661574ea37a12e798ff2dfb9539674f11a
Egor Chervonikov <crusaderworkvii@gmail.com>
2023-09-06 16:23:56 +0300

    I added home component.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,17 +1,24 @@
 import {Routes} from '@angular/router'
 import { RegistrationComponent } from './login/registration.component';
 import { IssuerComponent } from './issuer/issuer.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 const routeConfig: Routes = [
     {
         path: '',
-        component: IssuerComponent
+        component: HomeComponent
     },
 
     {
         path: 'register',
         component: RegistrationComponent,
         title: 'Register'
+    },
+
+    {
+        path: 'markets',
+        component: IssuerComponent
     }
 ];
 
